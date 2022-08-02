e62cd16f07bb5b6ea190c21c52f6b155d07d9863
Usama <s249895@studenti.polito.it>
2022-08-02 22:47:43 +0200

    Feat: Modified the policy: Policy now working which denies creation of resources without tags

--- create_policy.sh
+++ create_policy.sh
@@ -1,7 +1,10 @@
 #!bin/bash
 
 # Define the policy to deny resource creation without tags
-az policy definition create --name tagging-policy --display-name "Enforce resource tagging policy" --description "This policy ensures all indexed resources in your subscription are tagged or else deny creation." --rules policy.json --mode Indexed
+az policy definition create --name tagging-policy --display-name "Enforce resource tagging policy" --description "This policy ensures all indexed resources in your subscription are tagged or else deny creation." --rules policy.json --mode Indexed --params param.json
 
 # Assign the created policy
-az policy assignment create --name tagging-policy --display-name "Enforce resource tagging assignment" --policy "tagging-policy"
+az policy assignment create --name tagging-policy --display-name "Enforce resource tagging assignment" --policy tagging-policy --params --% "{ \"tagName\": {\"value\": \"Name\"} }"
+
+# I used powershell so I had to use --% to escape the quotes, however it is not
+# needed in linux or mac.
